5e4f7cd1bc87be5dcfceb5b4eccdd5cf517cf940
Jonny Burger <jonathanburger11@gmail.com>
2026-02-17 10:28:35 +0100

    Update use-static-files.ts

--- packages/studio/src/components/use-static-files.ts
+++ packages/studio/src/components/use-static-files.ts
@@ -1,4 +1,5 @@
 import React, {createContext, useContext, useEffect, useState} from 'react';
+import {useRemotionEnvironment} from 'remotion';
 import {type StaticFile, getStaticFiles} from '../api/get-static-files';
 import {watchPublicFolder} from '../api/watch-public-folder';
 
@@ -8,14 +9,23 @@ export const StaticFilesProvider: React.FC<{
 	readonly children: React.ReactNode;
 }> = ({children}) => {
 	const [files, setFiles] = useState(() => getStaticFiles());
+	const env = useRemotionEnvironment();
 
 	useEffect(() => {
+		if (!env.isStudio) {
+			return;
+		}
+
+		if (env.isReadOnlyStudio) {
+			return;
+		}
+
 		const {cancel} = watchPublicFolder((newFiles) => {
 			setFiles(newFiles);
 		});
 
 		return cancel;
-	}, []);
+	}, [env.isStudio, env.isReadOnlyStudio]);
 
 	return React.createElement(
 		StaticFilesContext.Provider,

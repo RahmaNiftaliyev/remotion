508ee0b47ce578248a54e00b382f687f8e9b2e10
Jonny Burger <jonathanburger11@gmail.com>
2026-02-17 11:11:25 +0100

    Update Homepage.tsx

--- packages/promo-pages/src/components/Homepage.tsx
+++ packages/promo-pages/src/components/Homepage.tsx
@@ -24,7 +24,7 @@ export const NewLanding: React.FC<{
 }> = ({colorMode, setColorMode}) => {
 	return (
 		<ColorModeProvider colorMode={colorMode} setColorMode={setColorMode}>
-			<div className="w-full relative">
+			<div className="w-full relative bg-[var(--background)]">
 				<div style={{overflow: 'hidden'}}>
 					<div>
 						<BackgroundAnimation />

00467247a4348a59e9a84cc4d9cbec12eb9bef5b
JonnyBurger <jonathanburger11@gmail.com>
2026-02-19 22:12:23 +0100

    prettier

--- packages/cloudrun/src/functions/render-media-single-thread.ts
+++ packages/cloudrun/src/functions/render-media-single-thread.ts
@@ -140,7 +140,8 @@ export const renderMediaSingleThread = async (
 				height: body.forceHeight ?? composition.height,
 				width: body.forceWidth ?? composition.width,
 				fps: body.forceFps ?? composition.fps,
-				durationInFrames: body.forceDurationInFrames ?? composition.durationInFrames,
+				durationInFrames:
+					body.forceDurationInFrames ?? composition.durationInFrames,
 			},
 			serveUrl: body.serveUrl,
 			codec: body.codec,

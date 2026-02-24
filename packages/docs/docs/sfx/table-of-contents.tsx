f78368adb618c7c5bc9d73dfa76dad04efaa2ac1
Jonny Burger <jonathanburger11@gmail.com>
2026-02-24 10:54:32 +0100

    update

--- packages/docs/docs/sfx/table-of-contents.tsx
+++ packages/docs/docs/sfx/table-of-contents.tsx
@@ -1,20 +1,27 @@
-import React from "react";
-import { Grid } from "../../components/TableOfContents/Grid";
-import { TOCItem } from "../../components/TableOfContents/TOCItem";
+import React from 'react';
+import {Grid} from '../../components/TableOfContents/Grid';
+import {TOCItem} from '../../components/TableOfContents/TOCItem';
+import {PlayButton} from './PlayButton';
 
 export const TableOfContents: React.FC = () => {
-  return (
-    <div>
-      <Grid>
-        <TOCItem link="/docs/sfx/whip">
-          <strong>whip</strong>
-          <div>URL for a whip sound effect</div>
-        </TOCItem>
-        <TOCItem link="/docs/sfx/whoosh">
-          <strong>whoosh</strong>
-          <div>URL for a whoosh sound effect</div>
-        </TOCItem>
-      </Grid>
-    </div>
-  );
+	return (
+		<div>
+			<Grid>
+				<TOCItem link="/docs/sfx/whip">
+					<strong>whip</strong>
+					<div>URL for a whip sound effect</div>
+					<div style={{marginTop: 8}}>
+						<PlayButton src="https://remotion.media/whip.wav" size={32} />
+					</div>
+				</TOCItem>
+				<TOCItem link="/docs/sfx/whoosh">
+					<strong>whoosh</strong>
+					<div>URL for a whoosh sound effect</div>
+					<div style={{marginTop: 8}}>
+						<PlayButton src="https://remotion.media/whoosh.wav" size={32} />
+					</div>
+				</TOCItem>
+			</Grid>
+		</div>
+	);
 };
---
+++ packages/skills/skills/remotion/rules/sfx.md
@@ -0,0 +1,14 @@
+---
+name: sfx
+description: Including sound effects
+metadata:
+  tags: sfx, sound, effect, audio
+---
+
+# Sound effect rules
+
+## Prerequisites
+
+```bash
+npx remotion add @remotion/sfx
+```

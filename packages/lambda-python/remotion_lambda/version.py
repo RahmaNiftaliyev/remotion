236e0b57e5e07cd1d77d3a9b4cd4824908c65f65
Jonny Burger <jonathanburger11@gmail.com>
2026-03-25 15:52:58 +0100

    v4.0.440

--- packages/lambda-python/remotion_lambda/version.py
+++ packages/lambda-python/remotion_lambda/version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-module-docstring, missing-final-newline
-VERSION = "4.0.439"
+VERSION = "4.0.440"
--- packages/media-parser/src/version.ts
+++ packages/media-parser/src/version.ts
@@ -1,2 +1,2 @@
 // Automatically generated on publish
-export const VERSION = '4.0.439';
+export const VERSION = '4.0.440';

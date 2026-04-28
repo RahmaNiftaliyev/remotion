65de1666458ecbbb76f67179d42b317af7191eee
JonnyBurger <jonathanburger11@gmail.com>
2026-04-28 15:39:12 +0200

    beta.10

--- packages/cli/src/extra-packages.ts
+++ packages/cli/src/extra-packages.ts
@@ -1,9 +1,9 @@
 export const EXTRA_PACKAGES: Record<string, string> = {
-	mediabunny: '1.42.0-beta.9',
-	'@mediabunny/ac3': '1.42.0-beta.9',
-	'@mediabunny/mp3-encoder': '1.42.0-beta.9',
-	'@mediabunny/aac-encoder': '1.42.0-beta.9',
-	'@mediabunny/flac-encoder': '1.42.0-beta.9',
+	mediabunny: '1.42.0-beta.10',
+	'@mediabunny/ac3': '1.42.0-beta.10',
+	'@mediabunny/mp3-encoder': '1.42.0-beta.10',
+	'@mediabunny/aac-encoder': '1.42.0-beta.10',
+	'@mediabunny/flac-encoder': '1.42.0-beta.10',
 	zod: '4.3.6',
 };
 
